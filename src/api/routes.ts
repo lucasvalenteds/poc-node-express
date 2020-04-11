0cc217ac5979a37bf49e77e0caf28f2ee48c8ca5
Lucas <lucasvalenteds@gmail.com>
2020-04-11 20:33:27 -0300

    Enable Customer API routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import {
+  createCustomerRouter,
+  CustomerControllerDefault,
+  CustomerServiceDefault,
+} from "../customer";
 
 export function createExpressRouter(): Router {
-  return Router();
+  return Router().use(
+    createCustomerRouter(
+      new CustomerControllerDefault(new CustomerServiceDefault([]))
+    )
+  );
 }
